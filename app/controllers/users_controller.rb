4259f5d18d0fa92041203429b14d37514fbdcf21
yusa0730 <isizawa5226@gmail.com>
2019-05-16 15:26:56 +0900

    edit layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 	end
 	def update
 		@user = User.find(params[:id])
-		@user = update(user_params)
+		@user.update(user_params)
 		redirect_to user_path(@user.id)
 	end
 
